840ac39573256e49b61efcaec539e287b8f81f43
madhurajk <madhuraj.kumara@lucidatechnologies.com>
2023-09-28 09:38:51 +0530

    First

--- constants/constants.js
+++ constants/constants.js
@@ -1,35 +1,56 @@
-export const sortCategoryData = ['All', 'Popular', 'Recommended', 'More'];
-
 export const categoriesData = [
     {
-        title: 'Ocean',
-        image: require('../../assets/images/ocean.png')
+        title: 'Pharmacy',
+        image: require('../assets/Phar.jpg')
     },
     {
-        title: 'Mountain',
-        image: require('../../assets/images/mountain.png')
+        title: 'Diagnostics',
+        image: require('../assets/Diagnostics.jpg')
     },
     {
-        title: 'Camp',
-        image: require('../../assets/images/camp.png')
+        title: 'Doctor',
+        image: require('../assets/Doctor.png')
     },
     {
-        title: 'Sunset',
-        image: require('../../assets/images/sunset.png')
+        title: 'Factory Direct',
+        image: require('../assets/Factory.png')
     },
     {
-        title: 'Hiking',
-        image: require('../../assets/images/hiking.png')
+        title: 'Baby Needs',
+        image: require('../assets/Baby.jpg')
     },
     {
-        title: 'Beach',
-        image: require('../../assets/images/beach.png')
+        title: 'Personal Care',
+        image: require('../assets/Persnol.jpg')
     },
     {
-        title: 'Forest',
-        image: require('../../assets/images/forest.png')
+        title: 'Health & Nutrition',
+        image: require('../assets/Health.jpg')
     },
-    
+    {
+        title: 'OTC & Health Needs',
+        image: require('../assets/OTC.png')
+    },
+    {
+        title: 'Vitamins & Supplemets',
+        image: require('../assets/Vitamin.jpg')
+    },
+    {
+        title: 'Diabetic Needs',
+        image: require('../assets/Diabetic.png')
+    },
+    {
+        title: 'Household Needs',
+        image: require('../assets/Household.jpg')
+    },
+    {
+        title: 'Ayurvedic',
+        image: require('../assets/Ayurvedic.png')
+    },
+    {
+        title: 'MedPlus Payback Points',
+        image: require('../assets/MedPoint.jpg')
+    }
 ]
 export const destinationData = [
     {
@@ -40,7 +61,7 @@ export const destinationData = [
         price: 1200,
         shortDescription: "Osaka Castle is a Japanese castle in Chūō-ku, Osaka, Japan. The castle is one of Japan's most famous landmarks.",
         longDescription: "Osaka Castle is a Japanese castle in Chūō-ku, Osaka, Japan. The castle is one of Japan's most famous landmarks and it played a major role in the unification of Japan during the sixteenth century of the Azuchi-Momoyama period.",
-        image: require('../../assets/images/hotel.png')
+        image: require('../assets/hero2.jpg')
     },
     {
         title: 'Island Itsukushima Shrine',
@@ -50,7 +71,7 @@ export const destinationData = [
         price: 3000,
         shortDescription: "The Itsukushima shrine is one of Japan's most popular tourist attractions.",
         longDescription: "Itsukushima Shrine is a Shinto shrine on the island of Itsukushima, best known for its 'floating' torii gate. It is in the city of Hatsukaichi in Hiroshima Prefecture in Japan, accessible from the mainland by ferry at Miyajimaguchi Station.",
-        image: require('../../assets/images/island.png')
+        image: require('../assets/hero2.jpg')
     },
     
     {
@@ -61,7 +82,7 @@ export const destinationData = [
         price: 1000,
         shortDescription: "Babusar Top is a mountain pass in Pakistan at the north of the 150 km long in beautiful Kaghan Valley",
         longDescription: "Babusar Pass or Babusar Top is a mountain pass in Pakistan at the north of the 150 km long Kaghan Valley, connecting it via the Thak Nala with Chilas on the Karakoram Highway. It is the highest point in Kaghan Valley that can be easily accessed by cars.",
-        image: require('../../assets/images/camp.png')
+        image: require('../assets/hero2.jpg')
     },
     {
         title: 'Todaiji Temple',
@@ -71,6 +92,6 @@ export const destinationData = [
         price: 400,
         shortDescription: "Todaiji is one of Japan's most famous and significant temples and a landmark of Nara.",
         longDescription: "Tōdai-ji is a Buddhist temple complex that was once one of the powerful Seven Great Temples, located in the city of Nara, Japan. Though it was originally founded in the year 738 CE, Tōdai-ji was not opened until the year 752 CE.",
-        image: require('../../assets/images/forest.png')
+        image: require('../assets/hero2.jpg')
     },
 ]
--- screens/Home.js
+++ screens/Home.js
@@ -1,50 +1,62 @@
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Image, Platform, Dimensions, TextInput } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Image, Platform, Dimensions, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import { MagnifyingGlassIcon, ShoppingCartIcon } from 'react-native-heroicons/outline'
 import COLORS from '../constants/colors';
-// import Categories from '../components/categories';
-// import SortCategories from '../components/sortCategories';
-// import Destinations from '../components/destinations';
+import { MapPinIcon } from 'react-native-heroicons/solid'
+import { BellIcon } from 'react-native-heroicons/outline'
+import Categories from '../components/Categories';
+
 
 const {width, height} = Dimensions.get('window');
 
-const ios = Platform.OS=='ios';
-const topMargin = ios? 'mt-3': 'mt-10';
+const style = StyleSheet.create({
+  item:{
+    backgroundColor:'white'
+  }
+
+});
+
 const Home = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <ScrollView showsVerticalScrollIndicator={false} className={"space-y-6 "+topMargin}>
+      <ScrollView style={style.item}>
      
-        <View className="mx-5 flex-row justify-between items-center mb-10" >
-        <TouchableOpacity>
+        <View style={{flex:1, flexDirection:'row',flexWrap:'wrap'}} >
+            <TouchableOpacity>
             <Image source={require('../assets/doc.png')} style={{height: wp(8), width: wp(8), marginTop: height*0.05}} />
-          </TouchableOpacity>
-        {/* avatar */}
-          <Text style={{fontSize: wp(4)}} className="font-bold text-neutral-700">Banasankari,Bangalore</Text>
-          <TouchableOpacity>
-            <Image source={require('../assets/hero2.jpg')} style={{height: wp(12), width: wp(12), marginLeft:height*0.40}} />
-          </TouchableOpacity>
-        </View>
+              </TouchableOpacity>
+             <BellIcon size="34" color="black" style={{ marginTop: height*0.05, marginLeft:height*0.32}} />
+              <TouchableOpacity>
+             <ShoppingCartIcon size="35" color="blue" style={{ marginTop: height*0.05}} />
+            </TouchableOpacity>
+       </View>
 
+         <View style={{flex:1, flexDirection:'row',flexWrap:'wrap'}}>
+            <MapPinIcon size="25" color="green" />
+            <Text className="font-semibold text-base">
+              4th Cross, Banasankari, Bangalore 571613
+            </Text>
+            </View>
         
 {/* search bar */}
-<View className="mx-5 shadow" style={{marginTop: height*0.01}}>
+
+ <View className="mx-5 shadow" style={{marginTop: height*0.01}}>
           <View className="flex-row items-center rounded-full p-1 bg-[#e6e6e6]">
             <TextInput placeholder='Search Medicines & General products...' className="p-4 flex-1 font-semibold text-gray-700" />
             <TouchableOpacity 
               className="rounded-full p-2" 
               style={{backgroundColor: COLORS.secondary}}>
-              <MagnifyingGlassIcon size="40" strokeWidth={2} color="white" />
+              <MagnifyingGlassIcon size="40" strokeWidth={2} color="grey" />
             </TouchableOpacity>
           </View>
         </View>
 
+    <Text>{"\n"}</Text>
 
-        {/* categories */}
-        {/* <View className="mb-4">
+       {/* categories */}
+        <View className="mb-4">
           <Categories />
-        </View> */}
+        </View>
 
         {/* sort categories */}
         {/* <View className="mb-4">
@@ -56,7 +68,6 @@ const Home = () => {
           <Destinations />
         </View> */}
       </ScrollView>
-    </SafeAreaView>
   )
 }
 
